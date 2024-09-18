cd94dca574e7dbf280deda59449a900ab73a6116
Čeněk Použník <potuznikcenek@gmail.com>
2024-09-18 18:25:45 +0200

    backend pro zobrazeni patra

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,6 @@ app.secret_key = 'dev'
 app.config.from_pyfile('../instance/config.py')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-app.config['DB_SERVER'] = 'https://jsnsgekom.cz'
+app.config['DB_SERVER'] = 'http://localhost:5000'
 
 from app import routs
--- app/routs.py
+++ app/routs.py
@@ -1,6 +1,15 @@
 from app import app
 from flask import flash, render_template, redirect, url_for, jsonify, abort, request
+import requests
 
-@app.route('/')
-def index():
-    pass
+CURRENT_DAY = 1 # docasne
+
+@app.route('/patro/<floor>')
+def patro(floor):
+    try:
+        response = requests.get(app.config["DB_SERVER"]+f"/api/query/program_items/floor/{floor}?day={CURRENT_DAY}")
+    except requests.exceptions.RequestException as e:
+        return f"Error - server ({app.config["DB_SERVER"]}) is unreachable - {e}"
+    if not response.status_code == 200:
+        return f"Error - status code {response.status_code} received from server ({app.config["DB_SERVER"]})"
+    return render_template("floor.html", floor=floor, day=CURRENT_DAY, program=response.json())
